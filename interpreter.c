6a6e6a8703da838646df2cebe1ad222412bf61c0
mcclurgm <mcgearboulder@gmail.com>
2019-05-20 21:24:52 -0500

    Add to skeleton of interpreter.c; design planning

--- interpreter.c
+++ interpreter.c
@@ -3,25 +3,43 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <assert.h>
 
 void interpret(Value *tree) {
-
+    Value *current = tree;
+    while(current->type != NULL_TYPE) {
+        eval(current, )
+        current = cdr(current);
+    }
 }
 
 Value *eval(Value *expr, Frame *frame) {
-    if (expr->type == INT_TYPE) {
+    assert(expr != NULL);
 
-    } else if (....) {
+    // Primitive types
+    if (expr->type == INT_TYPE) {
+        // return expr->i;
+    } else if (expr->type == DOUBLE_TYPE) {
+        // return expr->d;
+    } else if (expr->type == STR_TYPE) {
+        // return expr->s;
+    } else if (expr->type == BOOL_TYPE) {
+        // return expr->i;
+    } else if (expr->type == NULL_TYPE) {
 
     } else if (expr->type == SYMBOL_TYPE) {
         lookUpSymbol(expr, frame);
-    } else if (expr->type == CONS_TYPE) {
-        // This means that the expression is a complete S-expression
+    }
+    // This means that the expression is a complete S-expression
+    else if (expr->type == CONS_TYPE) {
 
         Value *first = car(expr);
         Value *args = cdr(expr);
 
         //TODO: sanity and error checking on first...
+        // It should be a symbol type
+        // None of these pointers should be null
+        // cdr can be null_type (NOT NULL, which should be handled from above)
 
         // Special cases
         if (!strcmp(first->s, "if")) {
--- planning.md
+++ planning.md
@@ -45,5 +45,24 @@
   * `let`: list of bindings isn't a nested list
     * `(let ((x 1)) ...)` is good, not `(let (x 1) ...)`
   * If the first element of a binding pair isn't a symbol / identifier
+  * If a binding pair list is not of length 2
 * Unbinded variable
   * Should be handled in searching the frames
+
+## Frame handling
+
+* Each `Frame` has a pointer to its parent and the bindings (defined in `interpreter.h`)
+* If the parent is null, then we're at the global and any variable lookup has failed
+  * How do we implement this null??? A null pointer or something
+    * `parent = NULL;`
+    * This will break the idea of being able to just `assert(some pointer != NULL)` for dealing with frames only
+
+### Bindings
+
+* This is a linked list of binding pointers
+* Car of each element is a binding pair
+* A binding pair is a cons cell
+  * `car` is the variable name (`SYMBOL_TYPE` `Value`)
+  * `cdr` is the value it's bound to (arbitrary type as long as it's a valid value)
+  * We can make a `makeBinding(Value *name, Value *val)`
+    * `name` is a symbol type and you should just be able to pass it directly from the tree

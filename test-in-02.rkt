896ed59392aab0b981a773b98ffed5bccfc68972
Michael McClurg <michael.mcclurg@outlook.com>
2019-06-22 08:57:47 -0600

    Add tests for equal, cond

--- test-in-02.rkt
+++ test-in-02.rkt
@@ -1,4 +1,4 @@
-; Test boolean functions: cond, and, or, if, not
+; Test boolean functions: cond, and, or, if, not, equal, eq
 
 ; cond
 (cond
@@ -9,6 +9,19 @@
 (cond
     ((equal? 1 2) 2)
     ((equal? 1 1) 1))
+
+(define income-tax
+  (lambda (income)
+    (cond
+      [(<= income 10000) (* income .05)]
+      [(<= income 20000) (+ (* (- income 10000) .08) 500.00)]
+      [(<= income 30000) (+ (* (- income 20000) .13) 1300.00)]
+      [else (+ (* (- income 30000) .21) 2600.00)])))
+
+; (income-tax 5000) ; 250.0
+; (income-tax 15000) ; 900.0
+; (income-tax 25000) ; 1950.0
+; (income-tax 50000) ; 6800.0 
 
 ; and
 ; use side effects to test short circuit
@@ -33,3 +46,29 @@ avariable
 (not #t) ; #f
 (not #f) ; #t
 ; (not "false") ; #f
+
+; equal
+(equal? 1 1)
+(equal? 'a 3) ; #f
+(equal? #t 't) ; #f
+(equal? "abc" 'abc) ; #f
+(equal? "hi" '(hi)) ; #f
+(equal? #f '()) ; #f 
+(equal? 3.4 53344) ; #f
+(equal? 3 3.0) ; #f
+(equal? 3.4 (+ 3.0 .4)) ; #t
+(let ([x (* 12345678987654321 2)])
+  (equal? x x)) ; #t
+(equal? #t #t) ; #t
+(equal? #f #f) ; #t
+(equal? #t #f) ; #f
+(equal? (null? '()) #t) ; #t
+(equal? (null? '(a)) #f) ; #t 
+
+(define a 3)
+(define b a)
+(equal? a b)
+
+; eq
+(eq? a b)
+(eq? a a)
